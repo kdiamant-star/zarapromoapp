5a5d843a6f484a178b3eca8cb3f1f3cd61e1fd3f
kdiamant-star <kdiamant@asu.edu>
2025-12-05 22:48:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import json
 
-st.title("👔👘Zara Item Promotion Decider🎩👗")
+st.title("👔 Zara Item Promotion Decider 👗")
 
 ENDPOINT_URL = st.secrets["DATABRICKS"]["ENDPOINT_URL"]
 TOKEN = st.secrets["DATABRICKS"]["TOKEN"]
